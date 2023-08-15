4d253eda4918b41ca0ec9e2f68d180741d7bd587
Ryan <ryanmahara@gmail.com>
2023-08-15 11:45:15 -0500

    add simple express backend

---
+++ src/server/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Test test is this thing on');
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port: ${port}`);
+});
