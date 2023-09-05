a8b1481111a8795626eca6e2a60b80b841e1445e
Ryan <ryanmahara@gmail.com>
2023-09-05 13:53:38 -0500

    add prettier config and eslint prettier integration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,12 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    // Turns off all rules that are unnecessary or might conflict with Prettier - eslint-config-prettier
+    'prettier',
+  ],
   // .files and .directory's are ignored by default
   ignorePatterns: [],
   parser: '@typescript-eslint/parser',
